4cada58d26c472cc9adb9ee380756742fe8b1fa6
Anish Singh Walia <loveeanish@gmail.com>
2018-06-03 20:46:08 +0530

    recursive solution handling all corner cases

--- Programs/Pow(x,n).cpp
+++ Programs/Pow(x,n).cpp
@@ -1,3 +1,33 @@
+//efficient recursive solution handling all corner cases
+double myPow(double x, int n) {
+        
+        if(n==0)
+        {
+            return 1.0;
+        }
+        
+        //most important corner case
+        if(n == INT_MIN)
+        {
+            double res = myPow(x,n/2);
+            return (res*res);
+        }
+        
+        
+        //if power is negetive, then change n to positive
+        if(n < 0)
+        {
+            x = 1/x;
+            n = -n;
+        }
+        
+        double res1 = myPow(x,n/2);
+        
+        
+        //if power is even then 
+        return (n%2==0) ? res1*res1 : res1*res1*x; 
+  }
+
 double myPow(double x, int n) {
         
         double ans = 1;
