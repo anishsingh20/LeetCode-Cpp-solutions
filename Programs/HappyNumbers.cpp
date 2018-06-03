dd42b6b4fa2d27239b4b6ec386277ba58997726f
Anish Singh Walia <loveeanish@gmail.com>
2018-06-03 15:45:37 +0530

    optimal code for happy numbers in cpp

--- Programs/HappyNumbers.cpp
+++ Programs/HappyNumbers.cpp
@@ -1,1 +1,39 @@
-
+bool isHappy(int n) {
+       
+        
+        
+        int d = 0;
+          
+        if(n<0)
+            return false;
+  
+        int  loop = 20;//to prevernt looping infinitely, if input is not a happy number
+        
+        while(--loop)
+        {
+            
+         
+           int sum = 0;
+            
+            while(n)
+            {
+                d = n % 10;
+                sum += (d*d);
+                n  = n/10;
+             }
+            
+            //  re-setting n to the sum of square of digits
+            n = sum;    
+                   
+            //if 1 occurs , then break out of the loop
+            if(n==1)
+            {
+                return true;
+                break;
+            }
+         }
+        
+         return false;
+        
+ }
+        
