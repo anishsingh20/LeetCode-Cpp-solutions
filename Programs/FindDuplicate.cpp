a722af8a66db29094a7faa5399a947f88919c070
Anish Singh Walia <loveeanish@gmail.com>
2018-06-06 00:07:34 +0530

    optimized solution for finding duplicate

--- Programs/FindDuplicate.cpp
+++ Programs/FindDuplicate.cpp
@@ -22,3 +22,21 @@ unordered_set<int> s;
             
         }
     }
+
+
+//Another technique to find duplicate in the array using floyd's cycle detection algorithm-using constant space O(1) and O(n) time
+int findDuplicate(vector<int>& nums) {
+int slow=0,fast=0,t=0;
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow==fast) break;
+        }
+        while(true){
+            slow = nums[slow];
+            t = nums[t];
+            if(t==slow) break;
+        }
+        return t;
+       
+    }
