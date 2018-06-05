88570e5578649eaa357b98567b6d376d325b2889
Anish Singh Walia <loveeanish@gmail.com>
2018-06-05 13:52:35 +0530

    another method to find starting node of the cycle in list

--- Data structures/ReturnCycleHeadInLinkedList.c
+++ Data structures/ReturnCycleHeadInLinkedList.c
@@ -25,3 +25,32 @@ struct ListNode *detectCycle(struct ListNode *head) {
     
     
 }
+
+
+struct ListNode *detectCycle(struct ListNode *head) {
+//Cpp another solution-using a hash map-unordered set or set-
+unordered_set<ListNode *>m;
+	
+	
+	while(head)
+	{
+		
+		//if a cycle found, if a previous visited node is found-
+        //if there is already visited node in hash map, then there is a cycle and the find() function returns the iterator to the 
+        //already visited node.And that node is the starting node of the cycle
+		unordered_set<ListNode *>::iterator lastNodeCycle = m.find(head);
+		if(lastNodeCycle != m.end())
+		{
+			ListNode *first = *lastNodeCycle;
+			return first;
+		}
+		
+		//otherwise simply push inside the map
+		m.insert(head);
+		head = head->next;
+	}
+	
+	return NULL;
+        
+    }
+//T(n) = 16s
