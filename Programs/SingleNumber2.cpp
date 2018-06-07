09694271b4f5560df2aac625efe8c683ea94520c
Anish Singh Walia <loveeanish@gmail.com>
2018-06-07 13:53:15 +0530

    another optimal simple solution without using Bit manipulation

--- Programs/SingleNumber2.cpp
+++ Programs/SingleNumber2.cpp
@@ -8,4 +8,18 @@ int a = 0, b = 0;
 			a = (a ^ i) & ~b;
 		}
 		return b;
-    }
+    } //runtime = 10ms
+
+
+//another solution for finding the element which occurs only once.
+int singleNumber(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+        
+        for(int i = 0 ; i< nums.size();i+=3)
+        {
+            if(nums[i]-nums[i+1]!=0)
+                return nums[i];
+        }
+	
+}
