22e5fd684db7fa6659f3aefcb5ed06a78b7f1420
Anish Singh Walia <loveeanish@gmail.com>
2018-06-07 12:22:30 +0530

    second optimal method to find a different string

--- Programs/FindDifferenceInStrings.cpp
+++ Programs/FindDifferenceInStrings.cpp
@@ -18,3 +18,37 @@ char findTheDifference(string s, string t) {
         return (char)ans;
         
     }
+
+
+//method 2- Similar to Finding the only single occuring number in an array
+//concatenating both the strings, sorting them will make the same chars next to each other.The 2 same chars will occupy the adjacent locations, and the differenc between the is 0.
+//if we encounter any such char which is not equal to its next intermediate char, then it is the odd one out.
+char findTheDifference(string s, string t) {
+      
+        string res = (s+t);//concatenating both strings into 1
+        
+        // int size = res.size()-1;
+        
+         sort(res.begin(),res.end());
+        
+//         int ans = 0;
+        
+//         for(auto a : res)
+//         {
+//             ans ^= a;
+//         }
+//         return (char)ans;
+            int i=0;
+            for(; i < res.size()-1 ; i+=2)
+            {
+                if(res[i]-res[i+1] != 0)
+                {
+                    return res[i];
+                    
+                }
+            }
+        
+        return res[i];
+    
+        
+    }
