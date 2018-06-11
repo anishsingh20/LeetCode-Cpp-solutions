2d68c39de6a71d7efc3356c948a79f91e473d28b
Anish Singh Walia <loveeanish@gmail.com>
2018-06-11 13:30:01 +0530

    approach 1 to reverse elements in between list

--- Data structures/ReverseListBetween.cpp
+++ Data structures/ReverseListBetween.cpp
@@ -1,1 +1,23 @@
-
+//approach 1- reversing the pointers
+ListNode* reverseBetween(ListNode* head, int m, int n) {
+        if (!head)  return head;
+        ListNode* tmp_head = new ListNode(1);
+        tmp_head->next = head;
+        
+        ListNode* p = tmp_head;
+        
+        for(int i=1;i<m;i++)
+            p = p->next;
+        ListNode* pm = p->next;
+        
+        
+        //p是翻转部分前的节点，pm是反转后的尾节点
+        
+        for(int i=0;i<(n-m);i++){
+            ListNode* tmp = pm->next;
+            pm->next = tmp->next;
+            tmp->next = p->next;
+            p->next = tmp;
+        }
+        return tmp_head->next;
+    }
