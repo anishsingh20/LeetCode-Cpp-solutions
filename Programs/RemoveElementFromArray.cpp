3f5d5f4068cc09022cd2421890e494313f526b4e
Anish Singh Walia <loveeanish@gmail.com>
2018-06-13 12:07:26 +0530

    method 3 to remove elements from an array

--- Programs/RemoveElementFromArray.cpp
+++ Programs/RemoveElementFromArray.cpp
@@ -32,3 +32,21 @@ int removeElement(vector<int>& nums, int val) {
         }
         
         return count;
+
+        
+        //method3-
+        int removeElement(vector<int>& nums, int val) {
+        
+      int i = 0;
+    int n = nums.size();
+    while (i < n) {
+        if (nums[i] == val) {
+            nums[i] = nums[n - 1];
+            // reduce array size by one
+            n--;
+        } else {
+            i++;
+        }
+    }
+    return n;
+    }
