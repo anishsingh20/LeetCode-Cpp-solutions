fe50ca3bd6d103c073b628fbf4b03743cc6b57b6
Anish Singh Walia <loveeanish@gmail.com>
2018-06-14 19:07:11 +0530

    method 1 to remove duplicates from a sorted array

--- Programs/RemoveDuplicatesFromSortedArray.cpp
+++ Programs/RemoveDuplicatesFromSortedArray.cpp
@@ -1,1 +1,19 @@
-
+//Method-2)Not efficient and not in-place as consumes O(n) extra memory due to using set in cpp
+ int removeDuplicates(vector<int>& nums) {
+        
+        set<int>s;
+        for(int i = 0 ; i < nums.size(); i++)
+        {
+            s.insert(nums[i]);
+        }
+        
+       int j = 0 ;
+        
+      
+        for(set<int>::iterator i = s.begin() ; i!=s.end() ; i++)
+             nums[j++] = *i;
+            
+               
+        
+        
+        return j;
