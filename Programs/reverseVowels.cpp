823f49426b55205b14200b17276d3fb0218cf5d6
Anish Singh Walia <loveeanish@gmail.com>
2018-06-16 14:56:48 +0530

    program to reverse only vowels of a string

--- Programs/reverseVowels.cpp
+++ Programs/reverseVowels.cpp
@@ -1,1 +1,27 @@
-
+//optimal solution to reverse vowels of a string in cpp
+string reverseVowels(string s) {
+        int i = 0 ;
+        int j = s.size()-1;
+        
+        
+        
+        while(i<j)
+        {
+            if(s[i]=='a' || s[i]=='e' || s[i] == 'i' || s[i]=='o' || s[i]=='u' || 
+               s[i]=='A' || s[i]=='E' || s[i] == 'I' || s[i]=='O' || s[i]=='U')
+            {
+                if(s[j]=='a' || s[j]=='e' || s[j] == 'i' || s[j]=='o' || s[j]=='u' || 
+                   s[j]=='A' || s[j]=='E' || s[j] == 'I' || s[j]=='O' || s[j]=='U')
+                        swap(s[i++],s[j--]);
+                    
+                
+                else j--;
+               
+            }
+            
+            else i++;
+            
+        }
+        
+        return s;
+    }
