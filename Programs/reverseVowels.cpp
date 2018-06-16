d53acadf36a9936c5c945fd930f298b122676bbe
Anish Singh Walia <loveeanish@gmail.com>
2018-06-16 15:11:11 +0530

    another method 3 to reverse vowels of a string

--- Programs/reverseVowels.cpp
+++ Programs/reverseVowels.cpp
@@ -43,3 +43,46 @@ string reverseVowels(string s) {
         
         
     }
+
+
+//Method-3
+
+ bool isVowel(char c)
+    {
+        switch(c) 
+        {
+            case 'a' :
+            case 'e' :
+            case 'i' :
+            case 'o' :
+            case 'u' :
+            case 'A' :
+            case 'E' :
+            case 'I' :
+            case 'O' :
+            case 'U' :
+                return true;
+        }
+        
+        return false;
+    }
+
+string reverseVowels(string s) {
+        
+     int i = 0, j = s.size() - 1;
+       
+     while(i < j)
+     {
+         while(i < j  && !isVowel(s[i]))
+             i++;
+         
+         while(i<j && !isVowel(s[j]))
+             j--;
+         
+         swap(s[i++],s[j--]);
+     }
+        
+        
+    return s;
+        
+    }
