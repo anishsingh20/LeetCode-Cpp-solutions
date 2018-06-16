824524f2c30d00b89231d4be8b25df64e2c4ff74
Anish Singh Walia <loveeanish@gmail.com>
2018-06-16 12:16:49 +0530

    method 1 to replace all zeros to end of array

--- Programs/MoveZeroesToEnd.cpp
+++ Programs/MoveZeroesToEnd.cpp
@@ -1,1 +1,12 @@
 //program to shift all zeroes to the end of an array
+
+//Method-1 Using STL replace_if()
+
+void moveZeroes(vector<int>& nums) {
+        
+      
+      replace_if(remove(nums.begin(),nums.end(),0),nums.end(),[](int x){ return x!=0; },0);
+    
+     
+           
+    }
