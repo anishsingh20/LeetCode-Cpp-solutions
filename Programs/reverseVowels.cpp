3b747acefdd2fce4d2c9c930490c500bfd367601
Anish Singh Walia <loveeanish@gmail.com>
2018-06-16 15:02:43 +0530

    second method to reverse vowels

--- Programs/reverseVowels.cpp
+++ Programs/reverseVowels.cpp
@@ -25,3 +25,21 @@ string reverseVowels(string s) {
         
         return s;
     }
+
+
+//method-2 using find_first_of()  string function.
+
+ string reverseVowels(string s) {
+        
+         int i = 0, j = s.size() - 1;
+        while (i < j) {
+            i = s.find_first_of("aeiouAEIOU", i);
+            j = s.find_last_of("aeiouAEIOU", j);
+            if (i < j) {
+                swap(s[i++], s[j--]);
+            }
+        }
+        return s;
+        
+        
+    }
