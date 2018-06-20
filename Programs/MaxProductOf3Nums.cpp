ac10f74554ec949a4e08ffafc45fbc050a8d0c26
Anish Singh Walia <loveeanish@gmail.com>
2018-06-21 02:16:15 +0530

    slightly modified solution

--- Programs/MaxProductOf3Nums.cpp
+++ Programs/MaxProductOf3Nums.cpp
@@ -1,10 +1,13 @@
 //simple method to find maximum product of 3 numbers in an array using sorting-the array also has negetive inputs
 
-int maximumProduct(vector<int>& nums) {
+ int maximumProduct(vector<int>& nums) {
         
 
-        sort(nums.begin(),nums.end());
-        int size = nums.size()-1;
-        return max(nums[0]*nums[1]*nums[size],nums[size]*nums[size-1]*nums[size-2]);
+        sort(nums.begin(), nums.end());
         
+        //case when array has either all positive , or all negetive elements 
+        if (nums[0] >= 0 || nums[nums.size()-1] <= 0)
+            return nums[nums.size()-1]*nums[nums.size()-2]*nums[nums.size()-3];
+        
+        return max(nums[nums.size()-1]*nums[nums.size()-2]*nums[nums.size()-3], nums[0]*nums[1]*nums[nums.size()-1]);
     }
