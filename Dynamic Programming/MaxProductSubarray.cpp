38ff84dc70a851cad2ae1f787aaac3fde4d91e23
Anish Singh Walia <loveeanish@gmail.com>
2018-06-23 13:56:38 +0530

    second method to find max product of continious subarrays in O(1) space

--- Dynamic Programming/MaxProductSubarray.cpp
+++ Dynamic Programming/MaxProductSubarray.cpp
@@ -1,4 +1,27 @@
-//optimal O(n) solution using 2 arrays, one to store the maximum subarray product and another to store the minimum subarray products
+//method-1 without using aux arrays-simple O(n) time and O(1) extra space solution-
+int maxProduct(vector<int>& nums) {
+       
+        int res = nums[0];
+        
+        int curmax = nums[0],curmin = nums[0];
+        
+        for(int i = 1 ; i < nums.size() ; i++)
+        {
+            if(nums[i] < 0)
+                swap(curmax,curmin);
+            
+            curmax = max(nums[i],curmax*nums[i]);
+            curmin = min(nums[i],curmin*nums[i]);
+        
+            
+            res = max(res,curmax);
+        }
+        
+        return res;
+       
+    }
+
+//Method-2 :optimal O(n) solution using 2 arrays, one to store the maximum subarray product and another to store the minimum subarray products
 int maxProduct(vector<int>& nums) {
        
         vector<int>maxDp(nums.size(),nums[0]);//to store max subarray products
