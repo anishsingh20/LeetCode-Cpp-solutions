9f80e3aed776f3ad000be701acb46282ecdae847
Anish Singh Walia <loveeanish@gmail.com>
2018-06-24 13:56:59 +0530

    C program to find count of subarrays product less than K

---
+++ Dynamic Programming/SubarrayProductLessThanL.c
@@ -0,0 +1,21 @@
+//C solution which beats 100% submissions.
+int numSubarrayProductLessThanK(int* nums, int numsSize, int k) {
+    
+     
+        int count = 0 ;
+        int pro = 1;
+        
+        for(int i = 0,j = 0 ; j  < numsSize ; j++)
+        {
+            pro *= nums[j];
+            
+            //if product of subarray becomes greater than k, we reduce size of max-product < k window
+            while(i<=j && pro >= k)
+                pro /= nums[i++];
+            
+            count += (j-i+1);
+        }
+        
+        return count;
+    
+}
