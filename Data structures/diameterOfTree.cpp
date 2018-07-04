b82c0b7d14d2622b2e8e89637a23b027fecb523d
Anish Singh Walia <loveeanish@gmail.com>
2018-07-04 10:49:16 +0530

    another method to find diameter of binary tree

--- Data structures/diameterOfTree.cpp
+++ Data structures/diameterOfTree.cpp
@@ -1,3 +1,33 @@
+//O(n) solution i.e calculating height in the function itself-
+int diaUtil(TreeNode *root,int *height)
+    {
+        int lt=0,rt=0,ldia=0,rdia=0;
+        if(!root)
+        {
+            *height = 0;
+            return 0;
+        }
+        
+        
+        ldia = diaUtil(root->left,&lt);
+        rdia = diaUtil(root->right,&rt);
+        
+        
+        *height = max(lt,rt) + 1;
+        
+        return max(lt+rt,max(ldia,rdia));
+    }
+   
+    int diameterOfBinaryTree(TreeNode* root) {
+        
+        if(!root) return 0;
+        int ht = height(root);
+        int dia = diaUtil(root,&ht);
+        
+        return dia;
+        
+    }
+
 int height(TreeNode *root)
 {
     if(!root)
